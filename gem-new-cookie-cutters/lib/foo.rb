c1ed2540174192209598fd3c9e292afa1918e445
WebGet <webget@webget.com>
2009-02-26 14:14:19 -0800

    first commit

---
+++ gem-new-cookie-cutters/lib/foo.rb
@@ -0,0 +1,19 @@
+# = {camelize}
+#
+# Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
+# Copyright:: Copyright (c) 2006-2008 Joel Parker Henderson
+# License:: CreativeCommons License, Non-commercial Share Alike
+# License:: LGPL, GNU Lesser General Public License
+#
+#
+# ==Example
+#
+##
+
+class {camelize}
+
+  #
+  def 
+  end
+
+end
